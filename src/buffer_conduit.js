6cc5affd7b4a1a881af589435e183fc97071a8f5
Anders Pitman <tapitman11@gmail.com>
2019-02-19 17:33:34 -0700

    Draft BufferConduit implementation

--- src/buffer_conduit.js
+++ src/buffer_conduit.js
@@ -1,4 +1,96 @@
 class BufferConduit {
+  constructor(bufferSize) {
+    this._terminated = false
+    this._downstreamDemand = 0
+    this._buffered = []
+    this._bufferSize = bufferSize
+
+    this._requestCallback = () => {}
+    this._dataCallback = () => {}
+    this._endCallback = () => {}
+    this._terminationCallback = () => {}
+  }
+
+  _flush() {
+    while (this._downstreamDemand > 0 && this._buffered.length > 0) {
+      this._dataCallback(this._buffered.shift())
+      this._downstreamDemand--
+      this._requestCallback(1)
+    }
+  }
+
+  terminate() {
+    if (!this._terminated) {
+
+      this._terminated = true
+
+      if (this._pipee) {
+        this._pipee.terminate()
+      }
+      this._terminationCallback()
+    }
+  }
+
+  onTermination(callback) {
+    this._terminationCallback = callback
+  }
+
+  // Consumer
+  write(data) {
+    if (this._buffered.length < this._bufferSize) {
+      this._buffered.push(data)
+      this._requestCallback(1)
+    }
+    else {
+      throw new Error("BufferConduit: Attempt to write more than requested")
+    }
+
+    this._flush()
+  }
+
+  end() {
+    if (this._pipee) {
+      this._pipee.end()
+    }
+    this._endCallback()
+  }
+
+  onRequest(callback) {
+    this._requestCallback = callback
+    callback(this._bufferSize - this._buffered.length)
+  }
+
+
+  // Producer
+  request(numItems) {
+    this._downstreamDemand += numItems
+    this._flush()
+  }
+
+  pipe(consumer) {
+
+    this._pipee = consumer
+
+    this.onData((data) => {
+      consumer.write(data)
+    })
+
+    consumer.onRequest((numItems) => {
+      this.request(numItems)
+    })
+
+    consumer.onTermination(() => {
+      this.terminate()
+    })
+  }
+
+  onData(callback) {
+    this._dataCallback = callback
+  }
+
+  onEnd(callback) {
+    this._endCallback = callback
+  }
 }
 
 module.exports = {
--- test/test_buffer_conduit.js
+++ test/test_buffer_conduit.js
@@ -1,7 +1,209 @@
+const { expect } = require('chai')
 const { BufferConduit } = require('../')
 
 describe('BufferConduit', function() {
   it('has a constructor', function() {
     new BufferConduit()
+  })
+
+  it('initial demand equals buffer size', function(done) {
+    const buf = new BufferConduit(10)
+    const data = new Uint8Array(100).fill(24)
+
+    this.timeout(10)
+
+    buf.onRequest((numElements) => {
+      expect(numElements).to.equal(10)
+      done()
+    })
+  })
+
+  it('initial demand equals buffer size minus current buffered length', function(done) {
+    const buf = new BufferConduit(10)
+    const data = new Uint8Array(100).fill(24)
+
+    this.timeout(10)
+
+    buf.write(new Uint8Array(1))
+
+    buf.onRequest((numElements) => {
+      expect(numElements).to.equal(9)
+      done()
+    })
+  })
+
+  it('can write initial buffer worth', function() {
+    const buf = new BufferConduit(10)
+    for (let i = 0; i < 10; i++) {
+      buf.write(new Uint8Array({}))
+    }
+  })
+
+  it('writing more than buffer fails', function() {
+    const buf = new BufferConduit(1)
+    buf.write(new Uint8Array({}))
+    expect(() => {
+      buf.write(new Uint8Array({}))
+    }).to.throw("BufferConduit: Attempt to write more than requested")
+  })
+
+  it('can only write bufferSize + requested', function() {
+    const buf = new BufferConduit(3)
+    buf.request(2)
+    buf.write(new Uint8Array(1))
+    buf.write(new Uint8Array(1))
+    buf.write(new Uint8Array(1))
+    buf.write(new Uint8Array(1))
+    buf.write(new Uint8Array(1))
+    expect(() => {
+      buf.write(new Uint8Array(1))
+    }).to.throw("BufferConduit: Attempt to write more than requested")
+  })
+
+  it('passes data through', function() {
+    const buf = new BufferConduit(3)
+    const expected = [
+      new Uint8Array([1,2,3]),
+      new Uint8Array([4,5,6]),
+      new Uint8Array([7,8,9]),
+    ]
+
+    const observed = []
+    buf.onData((data) => {
+      observed.push(data)
+    })
+
+    buf.request(3)
+
+    buf.write(new Uint8Array([1,2,3]))
+    buf.write(new Uint8Array([4,5,6]))
+    buf.write(new Uint8Array([7,8,9]))
+
+    expect(observed).to.eql(expected)
+  })
+
+  it('can be ended', function(done) {
+    this.timeout(10)
+    const buf = new BufferConduit(1)
+
+    buf.onEnd(() => {
+      done()
+    })
+
+    buf.end()
+  })
+
+  it('can be terminated', function(done) {
+    this.timeout(10)
+    const buf = new BufferConduit(1)
+
+    buf.onTermination(() => {
+      done()
+    })
+
+    buf.terminate()
+  })
+
+  describe('piping', function() {
+
+
+    class DummyConsumer {
+      constructor() {
+        this._observed = []
+      }
+
+      getObserved() {
+        return this._observed
+      }
+
+      write(data) {
+        this._observed.push(data)
+      }
+
+      end() {
+        this._endCallback()
+      }
+
+      terminate() {
+        this._terminateCallback()
+      }
+
+      onRequest(callback) {
+        callback(3)
+      }
+
+      onEnd(callback) {
+        this._endCallback = callback
+      }
+
+      onTermination(callback) {
+        this._terminateCallback = callback
+      }
+    }
+
+
+    it('can be piped', function() {
+      const buf = new BufferConduit(1)
+      buf.pipe(new DummyConsumer())
+    })
+
+    it('passes data to pipee', function() {
+      const buf = new BufferConduit(1)
+
+      const expected = [
+        new Uint8Array([1,2,3]),
+        new Uint8Array([4,5,6]),
+        new Uint8Array([7,8,9]),
+      ]
+
+      const consumer = new DummyConsumer()
+
+      buf.pipe(consumer)
+
+      buf.write(new Uint8Array([1,2,3]))
+      buf.write(new Uint8Array([4,5,6]))
+      buf.write(new Uint8Array([7,8,9]))
+
+      expect(consumer.getObserved()).to.eql(expected)
+    })
+
+    it('end propagates to pipee', function(done) {
+      this.timeout(10)
+      const buf = new BufferConduit(1)
+      const consumer = new DummyConsumer()
+      buf.pipe(consumer)
+
+      consumer.onEnd(() => {
+        done()
+      })
+
+      buf.end()
+    })
+
+    it('terminate propagates to pipee', function(done) {
+      this.timeout(10)
+      const buf = new BufferConduit(1)
+      const consumer = new DummyConsumer()
+      buf.pipe(consumer)
+
+      consumer.onTermination(() => {
+        done()
+      })
+
+      buf.terminate()
+    })
+
+    it('pipee terminate propagates to buffer', function(done) {
+      this.timeout(10)
+      const buf = new BufferConduit(1)
+      const consumer = new DummyConsumer()
+      buf.pipe(consumer)
+
+      buf.onTermination(() => {
+        done()
+      })
+
+      consumer.terminate()
+    })
   })
 })
